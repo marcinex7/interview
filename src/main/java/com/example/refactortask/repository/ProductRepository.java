d5660f2bb8a237f6a2d4d0eb6aa24ebaebad95a1
Marcin Podolski <marcinex7@gmail.com>
2025-05-28 13:51:14 +0200

    fix: another fixes

--- src/main/java/com/example/refactortask/repository/ProductRepository.java
+++ src/main/java/com/example/refactortask/repository/ProductRepository.java
@@ -3,22 +3,13 @@ package com.example.refactortask.repository;
 import com.example.refactortask.model.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import java.math.BigDecimal;
-import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    List<Product> findByProductNameContainingIgnoreCase(String name);
+    @Override
+    @Query("SELECT p FROM Product p WHERE p.id = :id")
+    public Product getById(Long id);
 
-    @Query("SELECT p FROM Product p WHERE p.productPrice <= :price")
-    List<Product> findProductsCheaperThan(@Param("price") BigDecimal price);
-
-    @Query(value = "SELECT * FROM products p WHERE p.stock > 0", nativeQuery = true)
-    List<Product> findProductsInStock();
-
-    List<Product> findByCategoryCategoryId(Long categoryId);
 }
